f3bfa3df183b15536c4ad5f0a56a373bdc903ec9
vi000246 <vi000246@gmail.com>
2017-03-09 10:39:23 +0800

    完成回傳訊息判斷

--- test.py
+++ test.py
@@ -1,43 +1,20 @@
 # encoding: utf-8
-from Image import Image
-import requests
-import kerasInitModel
-import numpy as np
-import threading
-import time
-from concurrent import futures
-import multiprocessing
-import Queue
+class ReturnMsg:
+    captchaErr = "驗證碼錯誤"
+    IdErr = "身份證字號錯誤"
+    NoSeat = "此期間訂票額滿，\n或無指定條件之車次"
+    NoTrain = "【該時段、該車種已訂票額滿】\n─ 請改訂其他時段、車種乘車票"
+    DateErr = "訂票日期錯誤或內容格式錯誤"
+    NoReturn = "查無回傳資料"
+    @staticmethod
+    def success(match=None):
+        if match is not None:
+            return str.format("您的車票已訂到\n電腦代碼:{} \n車次:{}  車種:{}",
+                                match.group('code'), match.group('trainNumber'),
+                                match.group('kind').encode('utf-8'))
+        else:
+            return "您的車票已訂到"
 
-letters = list('0123456789')
+a = ReturnMsg.captchaErr
 
-
-start_time = time.time()
-
-def do():
-    model = kerasInitModel.LoadModel()
-    model.load_weights("model/model.h5")
-    req = requests.get('http://railway.hinet.net/ImageOut.jsp')
-    x = Image(req.content)
-    imgs = x.StartProcess()
-
-    data = np.empty((len(imgs), 50,50,3), dtype="float32")
-    for index,img in enumerate(imgs):
-        arr = np.asarray(img, dtype="float32") / 255.0  # 將黑白圖片轉成1,0陣列 原本是0,255
-        data[index, :, :, :] = arr
-
-    classes = model.predict_classes(data)
-    result = []
-    for c in classes:
-        result.append(letters[c])
-    print(''.join(result).upper())
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
-for i in range(10):
-    do()
-
-
-
-
+print(a is not ReturnMsg.captchaErr)
